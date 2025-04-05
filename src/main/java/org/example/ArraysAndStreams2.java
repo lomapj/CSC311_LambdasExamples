a6e8616f0336bbb1d210d4df7d121fe3cad1402d
Patrick Lomangino <lomapj@farmingdale.edu>
2025-04-04 22:31:00 -0400

    commit

--- src/main/java/org/example/ArraysAndStreams2.java
+++ src/main/java/org/example/ArraysAndStreams2.java
@@ -7,34 +7,47 @@ import java.util.stream.Collectors;
 
 public class ArraysAndStreams2 {
    public static void main(String[] args) {
-      String[] strings = 
-         {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+      String[] strings =
+              {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
 
       // display original strings
       System.out.printf("Original strings: %s%n", Arrays.asList(strings));
 
       // strings in uppercase
       System.out.printf("strings in uppercase: %s%n",
-         Arrays.stream(strings)             
-               .map(String::toUpperCase)   
-               .collect(Collectors.toList()));
+              Arrays.stream(strings)
+                      .map(String::toUpperCase)
+                      .collect(Collectors.toList()));
 
       // strings less than "n" (case insensitive) sorted ascending
       System.out.printf("strings less than n sorted ascending: %s%n",
-         Arrays.stream(strings)                            
-               .filter(s -> s.compareToIgnoreCase("n") < 0)
-               .sorted(String.CASE_INSENSITIVE_ORDER)
-               .collect(Collectors.toList()));             
+              Arrays.stream(strings)
+                      .filter(s -> s.compareToIgnoreCase("n") < 0)
+                      .sorted(String.CASE_INSENSITIVE_ORDER)
+                      .collect(Collectors.toList()));
 
       // strings less than "n" (case insensitive) sorted descending
       System.out.printf("strings less than n sorted descending: %s%n",
-         Arrays.stream(strings)
-               .filter(s -> s.compareToIgnoreCase("n") < 0)
-               .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
-               .collect(Collectors.toList()));
-   }
-} 
+              Arrays.stream(strings)
+                      .filter(s -> s.compareToIgnoreCase("n") < 0)
+                      .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                      .collect(Collectors.toList()));
 
+      System.out.printf("strings that start with a vowel: %s%n",
+              Arrays.stream(strings)
+                      .filter(s -> "aeiouAEIOU".indexOf(s.charAt(0)) != -1)
+                      .collect(Collectors.toList()));
+
+      String concatenated = Arrays.stream(strings)
+              .collect(Collectors.joining(", "));
+      System.out.printf("Concatenated string: %s%n", concatenated);
+
+      long count = Arrays.stream(strings)
+              .filter(s -> s.length() > 5)
+              .count();
+      System.out.printf("Number of strings with more than 5 characters: %d%n", count);
+   }
+}
 
 /**************************************************************************
  * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
